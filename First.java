02611757cd98d4099b9cda8a9039cd602faf7a53
OmkarBhurke <bhurkeomkar1@gmail.com>
2023-12-02 20:45:45 +0530

    first comment

---
+++ First.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class First{
+    public static void main(String[] args) {
+        System.out.println(".......First code run scuceesfully........");
+        System.out.println("Current date :" + new Date());
+    }
+}
