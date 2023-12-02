54cfe48bc205413a5c5ad1073fbeb64448fbb1b4
OmkarBhurke <bhurkeomkar1@gmail.com>
2023-12-02 20:49:35 +0530

    second commit

--- First.java
+++ First.java
@@ -4,5 +4,6 @@ public class First{
     public static void main(String[] args) {
         System.out.println(".......First code run scuceesfully........");
         System.out.println("Current date :" + new Date());
+        System.out.println("New line Added for checking ...........");
     }
 }
